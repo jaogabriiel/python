e0c2ce18765735102046c1e46b46df19e84ae88b
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-23 23:07:47 -0300

    fase 4 de python

---
+++ fase4.py
@@ -0,0 +1,17 @@
+print('Hello, World!')
+# # print(Olá, Mundo)
+print(7 + 4)
+print('7' + '4')
+7 + 4
+'7' + '4'
+# #print('olá' + 5)
+print('olá', 5)
+# nome = 'Gabriel'
+# idade = 19
+# peso = 99.7
+# print(nome, idade, peso) # a vírgula serve para mostrar mensagens e números, diferente do +
+# print(nome + idade + peso)
+nome = input('Qual é o seu nome?') # comando de entrada de dados
+idade = input('Qual anos você tem?')
+peso = input('Qunto você pesa?')
+print(nome, idade, peso)
---
+++ script-python/teste-scripts.py
@@ -0,0 +1,17 @@
+# nome = input('Qual seu nome?')
+# idade = input('Quantos anos você tem?')
+# peso = input('Qual seu peso?')
+# print(nome, idade, peso)
+
+nomeP = input('Qual seu nome?')
+print('Olá', nomeP, 'Prazer em te conhecer!')
+
+dia = input('Qual dia você nasceu?')
+mes = input('Qual mês você nasceu?')
+ano = input('Qual ano do seu nascimento?')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
+
+num1 = input('Digite um número ')
+num2 = input('Digite outro número ')
+sum = int(num1) + int(num2)
+print('A soma dos dois números é ', sum)

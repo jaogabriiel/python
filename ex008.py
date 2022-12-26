4e65b6a01137b70946680d2a608b15c20e01981c
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-26 19:00:41 -0300

    ex 8, 9 e 10

---
+++ ex008.py
@@ -0,0 +1,4 @@
+m = float(input('Digite um valor em metros: '))
+c = m * 100
+mm = m * 1000
+print('O valor em centímetros é: {:.0f} cm \n em milímetros é: {:.0f} mm'.format(c, mm))
---
+++ ex010.py
@@ -0,0 +1,3 @@
+d = float(input('Quanto você tem?'))
+dolar = d / 5.22
+print('Você pode comprar {:.2f}US$ de dólar'.format(dolar))

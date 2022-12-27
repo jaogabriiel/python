098c3f2cb7c3c4c9509cdb7a0dade864b44a91e4
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-27 18:45:46 -0300

    aprimorando os ex

--- ex008.py
+++ ex008.py
@@ -1,4 +1,8 @@
-m = float(input('Digite um valor em metros: '))
+m = float(input('Digite uma distância em metros: '))
+k = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
 c = m * 100
 mm = m * 1000
-print('O valor em centímetros é: {:.0f} cm \n em milímetros é: {:.0f} mm'.format(c, mm))
+print('O valor em Quilômetros é {}km \nem Hectômetro {}hm \nem Decâmetro {}dam \nem Decímetro {}dm \nem Centímetros {}cm e \nem Milímetros {}mm'.format(k, hm, dam, dm, c, mm))
--- ex009.py
+++ ex009.py
@@ -1,3 +1,15 @@
 num = int(input('Digite um valor: '))
-print('A tabuada do {} é'.format(num), end=' ')
-print(num * 1, num * 2, num * 3, num * 4, num * 5, num * 6, num * 7, num * 8, num * 9, num * 10)
+por1 = num  * 1
+por2 = num * 2
+por3 = num * 3
+por4 = num * 4
+por5 = num * 5
+por6 = num * 6
+por7 = num * 7
+por8 = num * 8
+por9 = num * 9
+por10 = num * 10
+print('------------------')
+print('A taduada de {} é'.format(num))
+print('{} X 1 = {} \n{} x 2 = {} \n{} x 3 = {} \n{} x 4 = {} \n{} x 5 = {} \n{} x 6 = {} \n{} x 7 = {} \n{} x 8 = {} \n{} x 9 = {} \n{} x 10 = {}'.format(num, por1, num, por2, num, por3, num, por4, num, por5, num, por6, num, por7, num, por8, num, por9, num, por10))
+print('------------------')

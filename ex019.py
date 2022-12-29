a2d42ff41d41669e8ef4a69e3634ec3b77f026c9
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-29 19:29:38 -0300

    up

--- ex019.py
+++
@@ -1,7 +0,0 @@
-Gabriel = 1
-Pedro = 2
-Lucas = 3
-Maria = 4
-import random
-sorteado = random.randint(1, 4)
-print(sorteado)
---
+++ exerciciopython/ex020.py
@@ -0,0 +1,9 @@
+from random import shuffle
+n1 = input('Primeiro aluno: ')
+n2 = input('Segundo aluno: ')
+n3 = input('Terceira aluno: ')
+n4 = input('Quarto aluno: ')
+nomes = [n1, n2, n3, n4]
+shuffle(nomes)
+print('A oredem da apresentação será:')
+print(nomes)

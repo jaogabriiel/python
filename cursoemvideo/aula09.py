e763cd7cb9fb444b10b5b33a60b4c1af6287a4fa
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-31 11:09:14 -0300

    manipulando strings

---
+++ cursoemvideo/aula09.py
@@ -0,0 +1,11 @@
+frase = 'Curso em Vídeo Python'
+print(frase[::2])
+
+print('Oi')
+
+print("""Welcome! Are you completely new to programming?
+If not then we presume you will be looking for information
+about why and how to get started with Python. Fortunately
+an experienced programer in any programming language
+(whatever it may be) can pick up Python very quickly.
+Its also easy for beginners to use and learn, so jump in!""")

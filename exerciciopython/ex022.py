d264acc72cc1d6b3caec0a90ae50ef0920df025a
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-31 12:01:55 -0300

    ex 22

---
+++ exerciciopython/ex022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Nome em maiúsculas: {}'.format(nome.upper()))
+print('Nome em minúsculas: {}'.format(nome.lower()))
+print('O nome possui um total de {} letras'.format(len(nome) - nome.count(' ')))
+dividido = nome.split()
+print('Seu primeiro nome é {} e possui {} letras'.format(dividido[0], len(dividido[0])))

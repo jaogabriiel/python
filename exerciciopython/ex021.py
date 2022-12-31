a9c5cf9661e947a45209a0da4b6c15de68c03448
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-30 23:15:35 -0300

    up

--- exerciciopython/ex021.py
+++ exerciciopython/ex021.py
@@ -1,6 +1,2 @@
-import pygame
-pygame.init()
-pygame.mixer.music.load(input('ex021.mp3'))
-pygame.mixer.music.play()
-input()
-pygame.event.wait()
+from playsound import playsound
+playsound("ex021.mp3")

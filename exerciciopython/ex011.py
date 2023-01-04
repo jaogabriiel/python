189e02cc91f5ecaa14ed58340e627a2b7f1964b8
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-04 19:45:56 -0300

    up

--- exerciciopython/ex011.py
+++ exerciciopython/ex011.py
@@ -2,4 +2,4 @@ lar = float(input('Digite a largura da parede: '))
 alt = float(input('Digite a altura da parede: '))
 area = lar * alt
 qtd_tinta = area / 2
-print('A dimensão da parede é {}x{} com uma áre de {:.2f}m², você precisará de {:.2}l de tinta para pintar a parede'.format(lar, alt, area, qtd_tinta))
+print('A dimensão da parede é {}x{} com uma áre de {:.2f}m², você precisará de {:.2f}l de tinta para pintar a parede'.format(lar, alt, area, qtd_tinta))

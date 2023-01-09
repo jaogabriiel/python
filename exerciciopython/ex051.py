578935354e35778c831887cb660c1aeaacbcd893
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-09 18:49:37 -0300

    ex 52

---
+++ exerciciopython/ex051.py
@@ -0,0 +1,6 @@
+primeiro = int(input('Digite o primeiro termo da P.A: '))
+razão = int(input('Digite a razão da P.A: '))
+décimo = primeiro + (10 - 1) * razão
+print('Os 10 primeiros termos da P.A são: ')
+for c in range(primeiro, décimo, razão):
+    print(c)

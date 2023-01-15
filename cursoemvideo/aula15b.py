133ddf49c1aeaaf546c0f7584372b605960bd112
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-15 09:57:27 -0300

    breake f string

---
+++ cursoemvideo/aula15b.py
@@ -0,0 +1,6 @@
+nome = 'José'
+idade = 33
+salário = 987
+print(f'O {nome} tem {idade} anos e ganha {salário:.2f}')
+#print('O {} tem {} anos'.format(nome, idade)) # PYTHON 3 
+#print('O %s tem %d anos' % (nome, idade)) #PYTHON 2

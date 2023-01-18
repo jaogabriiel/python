94bde87945d1e71eb1129b456eec2ab804840ff5
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-18 20:10:31 -0300

    up

--- exerciciopython/ex075.py
+++ exerciciopython/ex075.py
@@ -6,7 +6,7 @@ números = (n1, n2, n3, n4)
 print(f'Os números digitados foram {números}')
 print(f'O número 9 apareceu {números.count(9)} vezes')
 if 3 in números:
-    print(f'O primeiro 3 apareceu primeiramente na {números.index(3) + 1}° posição')
+    print(f'O número 3 apareceu primeiramente na {números.index(3) + 1}° posição')
 else:
     print('O valor 3 não foi encontrado')
 print('Os valores pares digitados foram: ', end='')

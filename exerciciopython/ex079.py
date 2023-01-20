c42788e0c3793775779262cdc7fb9dfaba96c06e
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-19 22:28:21 -0300

    ex 79

---
+++ exerciciopython/ex079.py
@@ -0,0 +1,14 @@
+lista = []
+resp = ' '
+while True:
+    num = int(input('Digite um número: '))
+    if num not in lista:
+        lista.append(num)
+    else:
+        print('O número já está na lista. Não será considerado')
+    resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if resp in 'N':
+        break
+print(f'Os números informados foram {lista}')

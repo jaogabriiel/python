3bffac57b9f97b5144808c8405a057de18d99642
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-19 21:15:21 -0300

    ex 78

---
+++ exerciciopython/ex078.py
@@ -0,0 +1,22 @@
+lista = []
+maior = 0
+menor = 0
+for c in range(0, 5):
+    lista.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        maior = menor = lista[c]
+    elif lista[c] > maior:
+        maior = lista[c]
+    elif lista[c] < menor:
+        menor = lista[c]
+
+print('=-'*20)
+print(lista)
+print(f'O maior valor digitado foi {maior} nas posições', end=' ')
+for i, v in enumerate(lista):
+    if v == maior:
+        print(f'{i}', end=' ')
+print(f'\nO menor valor digitador foi {menor} nas posições', end=' ')
+for i, v in enumerate(lista):
+    if v == menor:
+        print(f'{i}', end=' ')

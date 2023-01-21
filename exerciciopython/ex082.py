e910abd72b2c3038ea61eaf5ae177c682901d409
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-20 23:11:36 -0300

    ex 82

---
+++ exerciciopython/ex082.py
@@ -0,0 +1,19 @@
+num = []
+pares = []
+ímpares = []
+while True:
+    valor = int(input('Digite um valor: '))
+    num.append(valor)
+    resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if resp in 'N':
+        break
+print(f'Os valores digitados foram {num}')
+for c in num:
+    if c % 2 == 0:
+        pares.append(c)
+    else:
+        ímpares.append(c)
+print(f'Pares: {pares}')
+print(f'Ímpares: {ímpares}')

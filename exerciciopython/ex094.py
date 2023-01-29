cfa1b9fccc6ff7518f6633205e76b9470bf5787b
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-29 17:39:49 -0300

    up

--- exerciciopython/ex094.py
+++ exerciciopython/ex094.py
@@ -4,6 +4,7 @@ mulheres = list()
 acima = list()
 somaidade = 0
 while True:
+    print('=-'*40)
     cada['nome'] = str(input('Nome: '))
     cada['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
     while cada['sexo'] not in 'MmFf':
@@ -12,14 +13,18 @@ while True:
         mulheres.append(cada['nome'])
     cada['idade'] = int(input('Idade: '))
     somaidade = cada['idade'] + somaidade
-    pessoas.append(cada)
+    pessoas.append(cada.copy())
     resp = str(input('Deseja continuar? [S/N] ')).strip().lower()[0]
     while resp not in 'sn':
         resp = str(input('Deseja continuar? [S/N] ')).strip().lower()[0]
     if resp in 'n':
         break
 média = somaidade / len(pessoas)
+print('-'*40)
 print(f'Ao todo, foram cadastradas {len(pessoas)} pessoas')
 print(f'A média de idade do grupo é {média}')
 print(f'As mulheres cadastradas foram {mulheres}')
+for i, v in enumerate(pessoas):
+    if v['idade'] > média:
+        acima.append(v['nome'])
 print(f'As pessoas com idade acima da média são {acima}')

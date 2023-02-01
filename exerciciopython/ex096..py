2604c27895500667f5ab12d9d410f684a64a6df6
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-01-31 21:21:23 -0300

    área de um terreno com função

--- exerciciopython/ex096..py
+++ exerciciopython/ex096..py
@@ -1,10 +1,10 @@
-def área():
-    print('Controle de Terrenos')
-    print('-'*25)
-    largura = float(input('Largura do terreno (m): '))
-    comprimento = float(input('Comprimento do terreno (m): '))
-    area = largura * comprimento
+def área(x, y):
+    area = x * y
     print(f'Com uma dimensão {largura}x{comprimento} m² a área do terreno é {area}m²')
 
 
-área()
+print('Controle de Terrenos')
+print('-'*25)
+largura = float(input('Largura do terreno (m): '))
+comprimento = float(input('Comprimento do terreno (m): '))
+área(largura, comprimento)

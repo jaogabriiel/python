a4a0145112bc2e418d2ea70d4964bd3640e04501
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-02 20:32:40 -0300

    funções com retorno

--- cursoemvideo/aula21e.py
+++ cursoemvideo/aula21e.py
@@ -4,6 +4,6 @@ def somar(x = 0, y = 0, z = 0):
     
 r1 = somar(3, 2, 5) # o 'S' em return vai para dentro da variável 'r1'
 r2 = somar(2, 2)
-r3 = somar(6)
+r3 = somar(10)
 
 print(f'Os resultados foram {r1}, {r2} e {r3}')

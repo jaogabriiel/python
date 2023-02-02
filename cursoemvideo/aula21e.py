186c56d1f4e43581949d5a23186a34ceedb9c6de
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-02 20:32:12 -0300

    escopo de variável

---
+++ cursoemvideo/aula21e.py
@@ -0,0 +1,9 @@
+def somar(x = 0, y = 0, z = 0):
+    s = x + y + z
+    return s
+    
+r1 = somar(3, 2, 5) # o 'S' em return vai para dentro da variável 'r1'
+r2 = somar(2, 2)
+r3 = somar(6)
+
+print(f'Os resultados foram {r1}, {r2} e {r3}')

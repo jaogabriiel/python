e3b8d576a614be9251da8047d2aeef9107e38cb5
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-02 22:02:42 -0300

    função para votação

--- exerciciopython/ex101.py
+++ exerciciopython/ex101.py
@@ -1,3 +1,13 @@
-def voto()
+from datetime import date
+def voto(x):
+    idade = date.today().year - x
+    if idade < 18:
+        return 'NÃO VOTA'
+    elif 18 <= idade < 65:
+        return 'OBRIGATÓRIO'
+    else:
+        return 'OPCIONAL'
 
-voto(2003)
+ano = int(input('Digite o ano do seu nascimento: '))
+idade = date.today().year - ano
+print(f'Com {idade} voto: {voto(ano)}')

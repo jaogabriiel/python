4ebdd493fb937b4f95936749775edd08eb121011
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:19:04 -0300

    Média aritmética

--- exerciciopython/ex007.py
+++ exerciciopython/ex007.py
@@ -1,4 +1,4 @@
-n1 = float(input('Digite uma nota: '))
-n2 = float(input('Digite sua segunda nota: '))
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
 m = (n1 + n2) / 2
 print('A média final é: {:.1f}'.format(m))

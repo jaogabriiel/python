d963f0f7bf1aaba2d00b575fea88117ab4444745
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:31:10 -0300

    Analisador de textos

--- exerciciopython/ex022.py
+++ exerciciopython/ex022.py
@@ -3,4 +3,5 @@ print('Nome em maiúsculas: {}'.format(nome.upper()))
 print('Nome em minúsculas: {}'.format(nome.lower()))
 print('O nome possui um total de {} letras'.format(len(nome) - nome.count(' ')))
 dividido = nome.split()
+
 print('Seu primeiro nome é {} e possui {} letras'.format(dividido[0], len(dividido[0])))

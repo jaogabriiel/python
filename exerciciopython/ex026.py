38736be72bf813471b46929b389451fefdecee34
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:34:03 -0300

    Primeira e última ocorrência em uma string

--- exerciciopython/ex026.py
+++ exerciciopython/ex026.py
@@ -1,4 +1,5 @@
 frase = str(input('Digite uma frase: ')).strip()
+
 print('A letra "A" aparece {} vezes na frase'.format(frase.upper().count('A')))
 print('O primeiro "A" aparece na {}° posição'.format(frase.upper().find('A') + 1))
 print('O último "A" aparece na {}° posição'.format(frase.upper().rfind('A') + 1))

0b317be7eed83f154e3fda2a1281315fe325b61e
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:23:28 -0300

    Reajusto salarial

--- exerciciopython/ex013.py
+++ exerciciopython/ex013.py
@@ -1,4 +1,4 @@
 salário = float(input('Digite seu salário: R$'))
 aumento = 15 / 100 * salário
 salario_final = salário + aumento
-print('Seu salário de {}R$ com um aumento de 15% é de {:.2f}R$'.format(salário, salario_final))
+print('Seu salário de {}R$ com um aumento de 15% é de {:.1f}R$'.format(salário, salario_final))

35389e0d2ae8ea121263804a0a5c7ff23ce7f82c
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:28:51 -0300

    Seno, Cosseno e Tangente

--- exerciciopython/ex018.py
+++ exerciciopython/ex018.py
@@ -3,6 +3,7 @@ from math import sin, cos, tan, radians
 seno = sin(radians(ângulo))
 cosseno = cos(radians(ângulo))
 tangente = tan(radians(ângulo))
+
 print('O ângulo {} tem o seno de {:.2f}'.format(ângulo, seno))
 print('O ângulo {} tem o cosseno de {:.2f}'.format(ângulo, cosseno))
 print('O ângulo {} tem a tangente de {:.2f}'.format(ângulo, tangente))

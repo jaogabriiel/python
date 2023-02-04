7e21dfafe3ce4c83e20cb5df5ff453fb2e3c1a3d
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:22:55 -0300

    Cálculo de descontos

--- exerciciopython/ex012.py
+++ exerciciopython/ex012.py
@@ -1,4 +1,4 @@
 preco = float(input('Digite o valor do produto: R$'))
 desconto = 5 / 100 * preco
 pf = preco - desconto
-print('O preço final com desconto de 5% é {:.2f} R$'.format(pf))
+print('O preço final com desconto de 5% é {:.1f} R$'.format(pf))

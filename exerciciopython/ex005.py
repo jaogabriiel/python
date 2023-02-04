ed71d161250d06cc7b0637eecf2c927ba844c069
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:17:11 -0300

    antecessor e sucessor de um número

--- exerciciopython/ex005.py
+++ exerciciopython/ex005.py
@@ -1,2 +1,2 @@
 n = int(input('Digite um número: '))
-print('O seu antecessor é {} e seu sucessor é {}' .format(n - 1, n + 1))
+print('O seu antecessor é {} e seu sucessor é {}'.format(n - 1, n + 1))

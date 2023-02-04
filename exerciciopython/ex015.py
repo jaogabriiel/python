267f83ff5982aa3684e1557992e9f0deb19a7fea
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:24:42 -0300

    Cálculo de aluguel

--- exerciciopython/ex015.py
+++ exerciciopython/ex015.py
@@ -1,4 +1,5 @@
 dias = int (input('Quantos você ficou com o carro? '))
 km = float(input('Quantos km você andou? '))
 aluguel = (60 * dias) + (0.15 * km)
+
 print('O total a pagar é: {:.2f}R$'.format(aluguel))

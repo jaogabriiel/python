b473e20211cdefe4baf56357314d671886164f89
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 18:01:44 -0300

    analisando e gerando dicionários com função

--- exerciciopython/ex105.py
+++ exerciciopython/ex105.py
@@ -1,4 +1,12 @@
 def notas(* notas, sit=False):
+    """
+    Função que recebe recebe notas como parâmetro e retorna algumas informações, como:
+    -> Número de notas informadas
+    -> Maior nota
+    -> Menor nota
+    -> A média entre as notas
+    -> A situação (opcional)
+    """
     n = dict()
     n['total'] = len(notas)
     n['maior'] = max(notas)
@@ -15,3 +23,4 @@ def notas(* notas, sit=False):
 
 resp = notas(4, 8.5, 9.3, 10, sit=True)
 print(resp)
+help(notas)

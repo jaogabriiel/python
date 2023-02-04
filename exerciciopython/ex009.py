66e2350f8529c1390cdda800afe5ad564d90a57e
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:20:35 -0300

    Tabuada

--- exerciciopython/ex009.py
+++ exerciciopython/ex009.py
@@ -9,7 +9,7 @@ por7 = num * 7
 por8 = num * 8
 por9 = num * 9
 por10 = num * 10
-print('------------------')
+print('-------------------')
 print('A taduada de {} é'.format(num))
 print('{} X 1 = {} \n{} x 2 = {} \n{} x 3 = {} \n{} x 4 = {} \n{} x 5 = {} \n{} x 6 = {} \n{} x 7 = {} \n{} x 8 = {} \n{} x 9 = {} \n{} x 10 = {}'.format(num, por1, num, por2, num, por3, num, por4, num, por5, num, por6, num, por7, num, por8, num, por9, num, por10))
-print('------------------')
+print('-------------------')

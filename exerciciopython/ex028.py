1d297efc7d68a0efdac3e26d7aeac511a7812cc9
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:35:05 -0300

    Jogo de adivinhação

--- exerciciopython/ex028.py
+++ exerciciopython/ex028.py
@@ -2,6 +2,7 @@ from random import randint
 from time import sleep
 num = randint(0, 5)
 chute = int(input('Vou pensar em um número entre 0 e 5, será que você consegue adivinhá-lo? '))
+
 if chute == num:
     print('Espera aí rapidinho...')
     sleep(2)

22921c89567f1888d2ea4f1fd4da8db4d79a8cc9
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:34:41 -0300

    Primeiro e último nome de uma pessoa

--- exerciciopython/ex027.py
+++ exerciciopython/ex027.py
@@ -1,5 +1,6 @@
 nome = str(input('Digite seu nome completo: ')).strip()
 partido = nome.split()
+
 print('Muito prazer em te conhecer!')
 print('Seu primeiro nome é {}'.format(partido[0]))
 print('Seu último nome é {}'.format(partido[len(partido) - 1]))

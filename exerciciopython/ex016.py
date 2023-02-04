75680ef0460acac4ceba03dbce18fbdcdbc2f8e2
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:25:19 -0300

    Quebrando números

--- exerciciopython/ex016.py
+++ exerciciopython/ex016.py
@@ -1,5 +1,6 @@
 from math import trunc
 num = float(input('Digite um número: '))
+
 print('O valor digitado foi {} e a sua porção inteira é {} '.format(num, trunc(num)))
 
 '''num = float(input('Digite um númro: '))

c6cf55b6af2e467052140c49c22c758bb841df16
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:35:30 -0300

    Radar eletrônico

--- exerciciopython/ex029.py
+++ exerciciopython/ex029.py
@@ -1,4 +1,5 @@
 vel = float(input('Qual a velocidade do carro? '))
+
 if vel > 80:
     multa = (vel - 80) * 7
     print('MULTADO! Você ultrapassou a velocidade limite de 80Km/h você receberá uma multa de R${:.2f}'.format(multa))

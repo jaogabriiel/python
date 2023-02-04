c585ff967d48e6b7ed9a7b4fbf60b62e89b7fcee
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-04 20:18:15 -0300

    dobro, triplo e raiz quadrada de um número

--- exerciciopython/ex006.py
+++ exerciciopython/ex006.py
@@ -2,4 +2,4 @@ n = int(input('Digite um número: '))
 d = n * 2
 t = n * 3
 r = pow(n, 1/2)
-print('Seu dobro é {}, seu triplo é {} \ne sua raiz quadrada é {:.1f}'.format(d, t, r))
+print('Seu dobro é {}, seu triplo é {} \n e sua raiz quadrada é {:.1f}'.format(d, t, r))

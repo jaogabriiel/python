13298e507ea9731696e56776b698a9274888df94
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-05 18:37:31 -0300

    Somando ímpares múltiplos de 3

--- exerciciopython/ex048.py
+++ exerciciopython/ex048.py
@@ -4,4 +4,5 @@ for c in range(1, 501, 2):
     if c % 3 == 0:
         cont += 1
         soma = soma + c
+        
 print('A soma dos {} valores entre 1 e 500 que são múltiplos de 3 é {}'.format(cont, soma))

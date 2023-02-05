dcc96cecf76a806da88d900524a08bc6187f2d51
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2023-02-05 18:29:47 -0300

    Analisando triângulo

--- exerciciopython/ex035.py
+++ exerciciopython/ex035.py
@@ -1,6 +1,7 @@
 lado1 = float(input('Quanto mede o primeiro lado do triângulo? '))
 lado2 = float(input('Quanto mede o segundo lado? '))
 lado3 = float(input('Quanto mede o terceiro lado? '))
+
 if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
     print('Os segmentos acima podem formar um triângulo')
 else:
